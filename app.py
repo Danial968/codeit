46376c1729884b263ccc75b9c90af17090fe72a1
Nicolas Marc Wee <nicolaswee.2018@sis.smu.edu.sg>
2019-09-29 01:48:20 +0800

    asdfas
    asdf

--- app.py
+++ app.py
@@ -510,6 +510,12 @@ def player():
 
 @app.route('/prismo', methods = ["POST"])
 def prismo():
+
+    def depth(l):
+        if isinstance(l, list):
+            return 1 + max(depth(item) for item in l)
+        else:
+            return 0
     test = request.json
     initial = test["initial"]
     goal = test["goal"]
@@ -517,33 +523,82 @@ def prismo():
     moves = []
     output = {}
 
+    depthini = depth(initial)
+
     while(current != goal):
-        for i in range(len(current)):
-            if 0 in current[i]:
-                current_index = [i, current[i].index(0)]
-                break
-        num_at_goal = goal[current_index[0]][current_index[1]]
-        for i in range(len(current)):
-            if num_at_goal in current[i]:
-                to_move = [i, current[i].index(num_at_goal)]
-                break
-        if to_move[0] == current_index[0]:
-            if to_move[1] < current_index[1]:
+        if depthini == 1:
+            if 0 in current:
+                current_index = [current.index(0)]
+            num_at_goal = goal[current_index[0]]
+            if num_at_goal in current:
+                to_move = [current.index(num_at_goal)]
+            if to_move[0] < current_index[0]:
                 moves.append("L")
-                current[to_move[0]][to_move[1]], current[current_index[0]][current_index[1]] = current[current_index[0]][current_index[1]], current[to_move[0]][to_move[1]]
+                current[to_move[0]], current[current_index[0]] = current[current_index[0]], current[to_move[0]]
             else:
                 moves.append("R")
-                current[to_move[0]][to_move[1]], current[current_index[0]][current_index[1]] = current[current_index[0]][current_index[1]], current[to_move[0]][to_move[1]]
-        else:
-            if to_move[0] < current_index[0]:
-                moves.append("B")
-                current[to_move[0]][to_move[1]], current[current_index[0]][current_index[1]] = current[current_index[0]][current_index[1]], current[to_move[0]][to_move[1]]
+                current[to_move[0]], current[current_index[0]] = current[current_index[0]], current[to_move[0]]
+        elif depthini == 2:
+            for i in range(len(current)):
+                if 0 in current[i]:
+                    current_index = [i, current[i].index(0)]
+                    break
+            num_at_goal = goal[current_index[0]][current_index[1]]
+            for i in range(len(current)):
+                if num_at_goal in current[i]:
+                    to_move = [i, current[i].index(num_at_goal)]
+                    break
+            if to_move[0] == current_index[0]:
+                if to_move[1] < current_index[1]:
+                    moves.append("L")
+                    current[to_move[0]][to_move[1]], current[current_index[0]][current_index[1]] = current[current_index[0]][current_index[1]], current[to_move[0]][to_move[1]]
+                else:
+                    moves.append("R")
+                    current[to_move[0]][to_move[1]], current[current_index[0]][current_index[1]] = current[current_index[0]][current_index[1]], current[to_move[0]][to_move[1]]
             else:
-                moves.append("F")
-                current[to_move[0]][to_move[1]], current[current_index[0]][current_index[1]] = current[current_index[0]][current_index[1]], current[to_move[0]][to_move[1]]
+                if to_move[0] < current_index[0]:
+                    moves.append("B")
+                    current[to_move[0]][to_move[1]], current[current_index[0]][current_index[1]] = current[current_index[0]][current_index[1]], current[to_move[0]][to_move[1]]
+                else:
+                    moves.append("F")
+                    current[to_move[0]][to_move[1]], current[current_index[0]][current_index[1]] = current[current_index[0]][current_index[1]], current[to_move[0]][to_move[1]]
+        else:
+            for n in range(len(current)):
+                for i in range(len(current[n])):
+                    if 0 in current[n][i]:
+                        current_index = [n, i, current[n][i].index(0)]
+                        break
+            num_at_goal = goal[current_index[0]][current_index[1]][current_index[2]]
+            for n in range(len(current)):
+                for i in range(len(current[n])):
+                    if num_at_goal in current[n][i]:
+                        to_move = [n, i, current[n][i].index(num_at_goal)]
+                        break
+            if to_move[0] == current_index[0]:
+                if to_move[1] == current_index[1]:
+                    if to_move[2] < current_index[2]:
+                        moves.append("L")
+                        current[to_move[0]][to_move[1]][to_move[2]], current[current_index[0]][current_index[1]][current_index[2]] = current[current_index[0]][current_index[1]][current_index[2]], current[to_move[0]][to_move[1]][to_move[2]]
+                    else:
+                        moves.append("R")
+                        current[to_move[0]][to_move[1]][to_move[2]], current[current_index[0]][current_index[1]][current_index[2]] = current[current_index[0]][current_index[1]][current_index[2]], current[to_move[0]][to_move[1]][to_move[2]]
+                else:
+                    if to_move[1] < current_index[1]:
+                        moves.append("B")
+                        current[to_move[0]][to_move[1]][to_move[2]], current[current_index[0]][current_index[1]][current_index[2]] = current[current_index[0]][current_index[1]][current_index[2]], current[to_move[0]][to_move[1]][to_move[2]]
+                    else:
+                        moves.append("F")
+                        current[to_move[0]][to_move[1]][to_move[2]], current[current_index[0]][current_index[1]][current_index[2]] = current[current_index[0]][current_index[1]][current_index[2]], current[to_move[0]][to_move[1]][to_move[2]]
+            else:
+                if to_move[0] < current_index[0]:
+                    moves.append("D")
+                    current[to_move[0]][to_move[1]][to_move[2]], current[current_index[0]][current_index[1]][current_index[2]] = current[current_index[0]][current_index[1]][current_index[2]], current[to_move[0]][to_move[1]][to_move[2]]
+                else:
+                    moves.append("U")
+                    current[to_move[0]][to_move[1]][to_move[2]], current[current_index[0]][current_index[1]][current_index[2]] = current[current_index[0]][current_index[1]][current_index[2]], current[to_move[0]][to_move[1]][to_move[2]]
     output["moves"] = moves
     return jsonify(output)
-    
+
 @app.route('/defuse', methods = ["POST"])
 def defuse():
     input = request.json
--- primo.py
+++ primo.py
@@ -1,3 +1,9 @@
+def depth(l):
+    if isinstance(l, list):
+        return 1 + max(depth(item) for item in l)
+    else:
+        return 0
+
 def primo(test):
 
     initial = test["initial"]
@@ -6,46 +12,99 @@ def primo(test):
     current = initial
     moves = []
 
+    depthini = depth(initial)
+
     while(current != goal):
-        for n in range(len(current)):
-            for i in range(len(current[n])):
-                if 0 in current[n][i]:
-                    current_index = [n, i, current[i].index(0)]
+        if depthini == 1:
+            if 0 in current:
+                current_index = [current.index(0)]
+            num_at_goal = goal[current_index[0]]
+            if num_at_goal in current:
+                to_move = [current.index(num_at_goal)]
+            if to_move[0] < current_index[0]:
+                moves.append("L")
+                current[to_move[0]], current[current_index[0]] = current[current_index[0]], current[to_move[0]]
+            else:
+                moves.append("R")
+                current[to_move[0]], current[current_index[0]] = current[current_index[0]], current[to_move[0]]
+        elif depthini == 2:
+            for i in range(len(current)):
+                if 0 in current[i]:
+                    current_index = [i, current[i].index(0)]
                     break
-        num_at_goal = goal[current_index[0]][current_index[1]][current_index[2]]
-        for n in range(len(current)):
-            for i in range(len(current[n])):
-                if num_at_goal in current[n][i]:
-                    to_move = [n, i, current[i].index(num_at_goal)]
+            num_at_goal = goal[current_index[0]][current_index[1]]
+            for i in range(len(current)):
+                if num_at_goal in current[i]:
+                    to_move = [i, current[i].index(num_at_goal)]
                     break
-        if to_move[0] == current_index[0]:
-            if to_move[1] == current_index[1]:
-                if to_move[2] < current_index[2]:
+            if to_move[0] == current_index[0]:
+                if to_move[1] < current_index[1]:
                     moves.append("L")
                     current[to_move[0]][to_move[1]], current[current_index[0]][current_index[1]] = current[current_index[0]][current_index[1]], current[to_move[0]][to_move[1]]
                 else:
                     moves.append("R")
                     current[to_move[0]][to_move[1]], current[current_index[0]][current_index[1]] = current[current_index[0]][current_index[1]], current[to_move[0]][to_move[1]]
             else:
-                if to_move[1] < current_index[1]:
+                if to_move[0] < current_index[0]:
                     moves.append("B")
                     current[to_move[0]][to_move[1]], current[current_index[0]][current_index[1]] = current[current_index[0]][current_index[1]], current[to_move[0]][to_move[1]]
                 else:
                     moves.append("F")
                     current[to_move[0]][to_move[1]], current[current_index[0]][current_index[1]] = current[current_index[0]][current_index[1]], current[to_move[0]][to_move[1]]
         else:
-            if to_move[0] < current_index[0]:
-                moves.append("L")
+            for n in range(len(current)):
+                for i in range(len(current[n])):
+                    if 0 in current[n][i]:
+                        current_index = [n, i, current[n][i].index(0)]
+                        break
+            num_at_goal = goal[current_index[0]][current_index[1]][current_index[2]]
+            for n in range(len(current)):
+                for i in range(len(current[n])):
+                    if num_at_goal in current[n][i]:
+                        to_move = [n, i, current[n][i].index(num_at_goal)]
+                        break
+            if to_move[0] == current_index[0]:
+                if to_move[1] == current_index[1]:
+                    if to_move[2] < current_index[2]:
+                        moves.append("L")
+                        current[to_move[0]][to_move[1]][to_move[2]], current[current_index[0]][current_index[1]][current_index[2]] = current[current_index[0]][current_index[1]][current_index[2]], current[to_move[0]][to_move[1]][to_move[2]]
+                    else:
+                        moves.append("R")
+                        current[to_move[0]][to_move[1]][to_move[2]], current[current_index[0]][current_index[1]][current_index[2]] = current[current_index[0]][current_index[1]][current_index[2]], current[to_move[0]][to_move[1]][to_move[2]]
+                else:
+                    if to_move[1] < current_index[1]:
+                        moves.append("B")
+                        current[to_move[0]][to_move[1]][to_move[2]], current[current_index[0]][current_index[1]][current_index[2]] = current[current_index[0]][current_index[1]][current_index[2]], current[to_move[0]][to_move[1]][to_move[2]]
+                    else:
+                        moves.append("F")
+                        current[to_move[0]][to_move[1]][to_move[2]], current[current_index[0]][current_index[1]][current_index[2]] = current[current_index[0]][current_index[1]][current_index[2]], current[to_move[0]][to_move[1]][to_move[2]]
+            else:
+                if to_move[0] < current_index[0]:
+                    moves.append("D")
+                    current[to_move[0]][to_move[1]][to_move[2]], current[current_index[0]][current_index[1]][current_index[2]] = current[current_index[0]][current_index[1]][current_index[2]], current[to_move[0]][to_move[1]][to_move[2]]
+                else:
+                    moves.append("U")
+                    current[to_move[0]][to_move[1]][to_move[2]], current[current_index[0]][current_index[1]][current_index[2]] = current[current_index[0]][current_index[1]][current_index[2]], current[to_move[0]][to_move[1]][to_move[2]]    
     return moves
 test = {
-  "initial": [[ 1, 2, 3, 4],
-              [ 5, 6, 7, 8],
-              [ 9,10,12, 0],
-              [13,14,11,15]],
-  "goal": [[ 1, 2, 3, 4],
-           [ 5, 6, 7, 8],
-           [ 9,10,11,12],
-           [13,14,15, 0]]
+  "initial": [[[ 1, 2, 3],
+               [ 4, 0, 6],
+               [ 7, 8, 9]],
+              [[10,11,12],
+               [13, 5,14],
+               [16,17,15]],
+              [[19,20,21],
+               [22,23,24],
+               [25,26,18]]],
+  "goal": [[[ 1, 2, 3],
+            [ 4, 5, 6],
+            [ 7, 8, 9]],
+           [[10,11,12],
+            [13,14,15],
+            [16,17,18]],
+           [[19,20,21],
+            [22,23,24],
+            [25,26,0]]]
 }
 
 print(primo(test))
