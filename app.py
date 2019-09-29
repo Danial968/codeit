146575428318572fad671e7b2c7b1bd5bac76d07
Danial <danialshah968@gmail.com>
2019-09-29 10:04:03 +0800

    data

--- app.py
+++ app.py
@@ -462,7 +462,7 @@ def typing_contest():
 
 @app.route('/bucket-fill', methods = ["POST"])
 def bucketfill():
-    test = request
+    test = request.data
     print("hello")
     print(test)
     print("hihi")
